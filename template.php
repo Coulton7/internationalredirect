5a325c2e66a309213b27eab265d2fc601b0f7d8c
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-08-11 12:22:48 +0100

    Missed something off of templae which causes site not to work properly

--- template.php
+++ template.php
@@ -23,7 +23,7 @@ function internationalredirect_css_alter(&$css){
   $css['sites/all/themes/aesbs337/css/regions-style.css']['weight']=14;
   $css['sites/all/themes/aesbs337/css/block-style.css']['weight']=15;
   $css['sites/all/themes/aesbs337/css/field-style.css']['weight']=16;
-  $css['sites/all/themes/redirect/css/redirect-style.css']['weight']=187;
+  $css['sites/all/themes/internationalredirect/css/int-redirect-style.css']['weight']=187;
 }
 
 function internationalredirect_theme(){
